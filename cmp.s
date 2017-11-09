86ce8d3acad6ac7a9400078d9cd910dcdf0123d2
Tausif Zaman <tausifzmn4@gmail.com>
2017-11-09 09:08:13 -0600

    Update cmp.s

--- cmp.s
+++ cmp.s
@@ -10,9 +10,9 @@ main:
     PUSH {R1}
     BL _scanf
     POP {R1}
-    MOV R5, R2
+    MOV R5, R0
     MOV R2, R0
-    R0, #0
+    MOV R0, #0
     BL _part
     BL _printf
     B main
@@ -21,7 +21,7 @@ _part:
     PUSH {LR}
     
     CMP R1, #0
-    ADDEQ R1, #1
+    ADDEQ R0, #1
     POPEQ {PC}
     
     CMP R1, #0
@@ -55,6 +55,7 @@ _part:
 
 _scanf:
     PUSH {LR}                @ store LR since scanf call overwrites
+    PUSH {R1}
     SUB SP, SP, #4          @ make room on stack
     LDR R0, =format_str     @ R0 contains address of format string
     MOV R1, SP              @ move SP to R1 to store entry on stack
